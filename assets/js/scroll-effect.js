364cf23dda9026edff7cad3fd0cf5c8a1b4881be
FRANKLILD <37359629+FRANKLILD@users.noreply.github.com>
2020-09-12 19:04:00 -0700

    fix

--- assets/js/scroll-effect.js
+++ assets/js/scroll-effect.js
@@ -16,6 +16,10 @@ $(window).scroll(function () {
     });
 });
 
+
+/*
+    land in 
+*/
 $(window).scroll(function () {
     $('.land-effect').each(function () {
         var imagePos = $(this).offset().top;
@@ -23,7 +27,26 @@ $(window).scroll(function () {
         var topOfWindow = $(window).scrollTop();
         var scrollBottom = $(window).scrollTop() + $(window).height();
         if (imagePos < scrollBottom - imageHeight) {
+
+
+            /*
+                land in 
+            */
             $(this).addClass("landIn");
+            let landInTexts = document.querySelectorAll(".landIn");
+            landInTexts.forEach(landInText => {
+                let landin_letters = landInText.textContent.split("");
+                landInText.textContent = "";
+                landin_letters.forEach((letter, i) => {
+                    let landin_span = document.createElement("span");
+                    landin_span.textContent = letter;
+                    landin_span.style.animationDelay = `${i * 0.05}s`;
+                    landInText.append(landin_span);
+                });
+            });
+
+
+
         } else {
             $(this).removeClass("landIn");
         }
